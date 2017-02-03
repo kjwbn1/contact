564e1cbdcd2f44b44150a9eb9758d5c9335c4c31
EC2 Default User <kimjwally@naver.com>
2017-02-03 19:24:36 +0000

    컨트롤러와 뷰 만들고 라우트 지정하기

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contact::Application.config.secret_key_base = '45a0ecdd3c28ba4250d6ff527cb146bd7d2dc5555f7169338ad81944ee6801576e620b34770b74959c3b67984b7a86a2755512eb303d5d48a763d57bc864aaa4'
