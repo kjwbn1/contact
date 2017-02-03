c2ae150084851353ee095ff0a191ff498055500e
EC2 Default User <kimjwally@naver.com>
2017-02-03 20:37:28 +0000

    실제 이메일 전송 기능 탑재하기

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,7 +3,20 @@ class EmailController < ApplicationController
 	end
 
 	def email_send
-		@email = params[:email]
-		@content = params[:content]
+		email = params[:email]
+		title = params[:title]
+		content = params[:content]
+
+
+		gmail = Gmail.connect("kjwbn1@gmail.com","76058522dkaps!@")
+		gmail.deliver do
+			to email
+			subject title
+			text_part do
+				body content
+	end
+end
+
+	gmail.logout
 	end
 end
